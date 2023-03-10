22a351e39c60830521fd4a314165d3cf4d8a3f67
Abhijit <abhijeetawari1402@gmail.com>
2023-03-10 11:50:49 +0530

    add link to from react router dom to go to products page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,9 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-    return <h1>Welcome to HomePage</h1>
+    return <> <h1>Welcome to HomePage</h1>
+    <h4>Go to <Link to='/products'>products page</Link> </h4>
+    </>
 };
 
 export default HomePage;
